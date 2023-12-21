a33928e34fadf3176a532a85f5789f7e3e7f6da6
Jhon Patiño <146010485+DamianRoc13@users.noreply.github.com>
2023-12-21 10:36:23 -0500

    change

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -4,19 +4,19 @@ function Estudiante(nombre, correo, promedio) {
   this.nombre = nombre;
   this.correo = correo;
   this.promedio = promedio;
-  function validarPromedio(Estudiante) {
-    if (Estudiante.promedio >= 70) {
+  this.validarPromedio = function () {
+    if (this.promedio >= 70) {
       return "Aprobado";
     } else {
       return "Reprobado";
     }
-  }
-}
-function obtenerUsuario() {
-  let email = this.correo;
-  let post = email.indexOF("@");
-  let usuario = email.substring(0, post);
-  return usuario;
+  };
+  this.obtenerUsuario = function () {
+    let email = this.correo;
+    let post = email.indexOf("@");
+    let usuario = email.substring(0, post);
+    return usuario;
+  };
 }
 
 let damian = new Estudiante("Damian", "Damianolivo2018@gmail.com", 50);
